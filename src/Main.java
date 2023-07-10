69c51e1b367d6b9424d9a15cb91a65d679d13ff3
Olga <radenovic-family@yandex.ru>
2023-07-10 20:01:57 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+    public static void task1() {
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+
+
+    }
+    public static void task2() {
+
+    }
+    public static void task3() {
+
+    }
+    public static void task4 () {
+
+    }
+}
